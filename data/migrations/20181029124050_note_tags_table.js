a53a52720889b79da48cda8b99614ca7837b150f
Vera Simon <vera.simon01@gmail.com>
2018-10-29 12:58:32 -0700

    migrations
    Tinkered with the migration files a bit before running
    Added a couple of missed return in note_tags

--- data/migrations/20181029124050_note_tags_table.js
+++ data/migrations/20181029124050_note_tags_table.js
@@ -1,13 +1,17 @@
 exports.up = function(knex, Promise) {
-	tbl.increments();
-	tbl.integer('note_id')
-		.unsigned()
-		.references('id')
-		.inTable('notes');
-	tbl.integer('tag_id')
-		.unsigned()
-		.references('id')
-		.inTable('tags');
+	return knex.schema.createTable('note_tags', (tbl) => {
+		tbl.increments();
+		tbl.integer('note_id')
+			.unsigned()
+			.references('id')
+			.inTable('notes');
+		tbl.integer('tag_id')
+			.unsigned()
+			.references('id')
+			.inTable('tags');
+	});
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('note_tags');
+};

c1b7d4cc2eac3f1b75b5e6afa2a56c4d2d5563c3
Vera Simon <vera.simon01@gmail.com>
2018-10-29 15:05:27 -0700

    notesroutes.js
    Finished route for GET "/"

--- api/notesroutes.js
+++ api/notesroutes.js
@@ -10,7 +10,15 @@ const router = express.Router();
 
 // router.get('/', (req, res, next) => res.status(200).json({ message: 'Hello, world!' }));
 
-router.get('/', (req, res, next) => {});
+router.get('/', (req, res, next) => {
+	getNotes()
+		.then((notesList) => {
+			res.status(200).json(notesList);
+		})
+		.catch((err) => {
+			next(['h500', err]);
+		});
+});
 
 router.get('/:id', (req, res, next) => {});
 

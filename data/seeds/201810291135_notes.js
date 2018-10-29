7de2f947b42c910d4f20bfa293287b3a45b3dc3a
Vera Simon <vera.simon01@gmail.com>
2018-10-29 13:19:04 -0700

    notes seed
    Updated notes seed file

--- data/seeds/201810291135_notes.js
+++ data/seeds/201810291135_notes.js
@@ -1,13 +1,11 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+	return knex('notes')
+		.truncate()
+		.then(function() {
+			return knex('notes').insert([
+				{ title: 'rowValue1', text_body: 'rowValue1', _id: 'cc2cc3e9-5e20-5fbe-83e8-2ff155a7b993' },
+				{ title: 'rowValue2', text_body: 'rowValue2', _id: 'b573109c-02ed-5389-b402-9d5c06863183', __v: 2 },
+				{ title: 'rowValue3', text_body: 'rowValue3', _id: '4b947927-a164-5657-aac6-47b4d3697d03', __v: 7 }
+			]);
+		});
 };

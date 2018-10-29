126a60d2b14c031ef0138ead0b84b82ac891b5c3
Vera Simon <vera.simon01@gmail.com>
2018-10-29 10:51:35 -0700

    Boilerplate
    Added ./api/notesroutes.js
    Added ./data/models
    Simple test route for notesroutes.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const notesRoutes = require('../api/notesroutes');
 const { errorHandler } = require('../errorHandler/handlers');
 
 const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/notes', notesRoutes);
 
 server.use((req, res, next) => {
 	next(['h404', `The requested path '${req.path}' does not exist.`]);

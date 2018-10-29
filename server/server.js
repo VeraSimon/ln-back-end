aca4a69cb856d6e0d73aade1fa6996b8f62fe23d
Vera Simon <vera.simon01@gmail.com>
2018-10-29 10:36:11 -0700

    Boilerplate
    Added prod and dev packages
    yarn init
    knex init
    simple directory structure
    simple 404 route for non-existant routes
    wired up errorHandler
    updated package.json "scripts" and "jest"
    updated knexfile.js "development" and commented out "staging" and "production" for now
    basic dbconfig.js for dev

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const { errorHandler } = require('../errorHandler/handlers');
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use((req, res, next) => {
+	next(['h404', `The requested path '${req.path}' does not exist.`]);
+});
+
+server.use(errorHandler);
+
+module.exports = server;

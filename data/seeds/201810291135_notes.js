b96bc99c0952ec4a9ca4aec55cfead713a1d0c04
Vera Simon <vera.simon01@gmail.com>
2018-10-29 13:46:09 -0700

    seeds
    Finished "note_tags" seed
    When running "knex seed:run", ran into a bug with seeding not respecting .defaultTo() in "notes" migration, so updated it and associated seed accordingly

--- data/seeds/201810291135_notes.js
+++ data/seeds/201810291135_notes.js
@@ -3,7 +3,7 @@ exports.seed = function(knex, Promise) {
 		.truncate()
 		.then(function() {
 			return knex('notes').insert([
-				{ title: 'rowValue1', text_body: 'rowValue1', _id: 'cc2cc3e9-5e20-5fbe-83e8-2ff155a7b993' },
+				{ title: 'rowValue1', text_body: 'rowValue1', _id: 'cc2cc3e9-5e20-5fbe-83e8-2ff155a7b993', __v: 0 },
 				{ title: 'rowValue2', text_body: 'rowValue2', _id: 'b573109c-02ed-5389-b402-9d5c06863183', __v: 2 },
 				{ title: 'rowValue3', text_body: 'rowValue3', _id: '4b947927-a164-5657-aac6-47b4d3697d03', __v: 7 }
 			]);
--- data/seeds/201810291240_note_tags.js
+++ data/seeds/201810291240_note_tags.js
@@ -1,13 +1,12 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+	return knex('note_tags')
+		.truncate()
+		.then(function() {
+			return knex('note_tags').insert([
+				{ note_id: 1, tag_id: 2 },
+				{ note_id: 3, tag_id: 3 },
+				{ note_id: 2, tag_id: 1 },
+				{ note_id: 2, tag_id: 3 }
+			]);
+		});
 };

c643de054c6f1394ed80988ac2cc165a1a113f6a
Vera Simon <vera.simon01@gmail.com>
2018-10-30 08:42:21 -0700

    notesmodels.js
    Temporarily added logic to append an empty array to note.tags for getNotes(), getNotes(id), and postNote(newNote() until I implement full tags support
    Absentmindedly started implementing some DB schema changes before making this commit. Updated text_body to textBody to better mimick the original backend, and started work on removing id in favor of _id in "notes"

--- data/migrations/20181029113432_notes_table.js
+++ data/migrations/20181029113432_notes_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('notes', (tbl) => {
 		tbl.increments();
 		tbl.string('title', 255).notNullable();
-		tbl.string('text_body', 65535);
+		tbl.string('textBody', 65535);
 		tbl.uuid('_id')
 			.unique()
 			.notNullable();
--- data/models/notesmodels.js
+++ data/models/notesmodels.js
@@ -10,14 +10,36 @@ const getNotes = (id) => {
 		// 	.first();
 		return db('notes')
 			.where('_id', id)
-			.first();
+			.first()
+			.then((note) => {
+				// return db('actions')
+				//     .where('project_id', id)
+				//     .then((actions) => {
+				//         project.actions = actions;
+				//         return project;
+				//     })
+				//     .catch((err) => console.error('Model Error:\n', err));
+				// TODO: Implement tag fetching
+				note.tags = [];
+				return note;
+			})
+			.catch((err) => console.error('Model Error:\n', err));
 	} else {
 		// 	return db('notes_tags')
 		// 		.select('notes.id', 'notes.title', 'notes.text_body', 'tags.tag')
 		// 		.innerJoin('notes', 'notes.id', 'note_tags.note_id')
 		// 		.innerJoin('tags', 'tags.id', 'note_tags.tag_id')
 		// 		.orderBy('notes.id');
-		return db('notes');
+		return db('notes')
+			.then((noteList) => {
+				// TODO: Implement tag fetching
+				const list = noteList.map((note) => {
+					note.tags = [];
+					return note;
+				});
+				return list;
+			})
+			.catch((err) => console.error('Model Error:\n', err));
 	}
 };
 
@@ -30,7 +52,14 @@ const postNote = (newNote) => {
 
 	return db('notes')
 		.insert(newNote)
-		.into('notes');
+		.into('notes')
+		.then((note) => {
+			// TODO Implement tag fetching
+			// TODO Implement tag additions to DB
+			note.tags = [];
+			return note;
+		})
+		.catch((err) => console.error('Model Error:\n', err));
 };
 
 const putNote = (updatedNote) => {

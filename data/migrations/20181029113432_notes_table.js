ab336bb8f99da71fa99209f061e141a8cb1bc7d9
Vera Simon <vera.simon01@gmail.com>
2018-10-30 09:09:48 -0700

    #_notes_table.js migrations / #_notes.js seed / #_notes_table.js migration / #_notes.js seed
    Updated migration and seed files to facilitate the "notes" table using _id as the PK
    Removed defaultTo(0) from __v column in "notes" due to it incorrectly trying to assign a string "0" to an integer

--- data/migrations/20181029113432_notes_table.js
+++ data/migrations/20181029113432_notes_table.js
@@ -1,15 +1,13 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('notes', (tbl) => {
-		tbl.increments();
+		tbl.uuid('_id')
+			.primary()
+			.notNullable();
 		tbl.string('title', 255).notNullable();
 		tbl.string('textBody', 65535);
-		tbl.uuid('_id')
-			.unique()
-			.notNullable();
 		tbl.integer('__v')
-			.notNullable()
 			.unsigned()
-			.defaultTo(0);
+			.notNullable();
 	});
 };
 
